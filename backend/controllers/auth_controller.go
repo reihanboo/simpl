6608b9558c70b45f2b1ce98dc3f954b3b370c785
Joenathan <hazendas10@gmail.com>
2026-09-25 00:16:20 +0700

    fix: using cryptic libary to minimalize possibility of attacker brute force the OTP

--- backend/controllers/auth_controller.go
+++ backend/controllers/auth_controller.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
+	"crypto/rand"
 	"fmt"
 	"log"
-	"math/rand"
+	"math/big"
 	"net/http"
 	"os"
 	"time"
@@ -47,7 +48,12 @@ func Register(c *gin.Context) {
 	}
 
 	// Generate OTP
-	otpCode := fmt.Sprintf("%06d", rand.Intn(1000000))
+	n, err := rand.Int(rand.Reader, big.NewInt(1000000))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate OTP"})
+		return
+	}
+	otpCode := fmt.Sprintf("%06d", n.Int64())
 	expiresAt := time.Now().Add(10 * time.Minute)
 
 	// Create user
